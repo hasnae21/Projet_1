d6049a28e3b5596c9360c55a6e660adce0c1edee
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-11 13:35:20 +0100

    save

--- OOP_PHP_add/includes/signup.inc.php
+++ OOP_PHP_add/includes/signup.inc.php
@@ -1,10 +1,17 @@
 <?php 
 
 if(isset($_POST["submit"])){
-     
+   
+    
+//Grabbing the data  
 $uid= $_POST["uid"];
 $pwd= $_POST["pwd"];
-$pwdrepeat= $_POST["pwdrepeat"];
+$pwdrepeat= $_POST["pwdRepeat"];
 $email= $_POST["email"];
 
+//instantiate signupContr class /object for a class
+
+//running error handlers and user signup
+
+//going to back to front page
 }
