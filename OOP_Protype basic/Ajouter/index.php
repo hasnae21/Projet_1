058b452bfa01a8d7f3ccf0a7a3eeff786652350a
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-11 16:55:32 +0100

    s
    s

---
+++ OOP_Protype basic/Ajouter/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <center>
+        <h2>Add Promotion</h2>
+
+        <form method="post">
+                <label> Promotion name:</label>
+                <input type="text" placeholder="enter promotion name" />
+                <button type="submit"> Insert</button>
+        </form>
+    </center>
+
+    <br />
+
+    <div class="col-lg-12">
+        <div id="message"></div>
+        <div id="fetch"></div>
+    </div>
+
+</body>
+
+</html>
