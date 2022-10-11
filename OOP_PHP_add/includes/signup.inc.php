c6beed9fc0c9976e663d3336297af079bb42f897
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-11 12:59:58 +0100

    save
    save

---
+++ OOP_PHP_add/includes/signup.inc.php
@@ -0,0 +1,10 @@
+<?php 
+
+if(isset($_POST["submit"])){
+     
+$uid= $_POST["uid"];
+$pwd= $_POST["pwd"];
+$pwdrepeat= $_POST["pwdrepeat"];
+$email= $_POST["email"];
+
+}
---
+++ OOP_PHP_add/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login page</title>
+</head>
+
+<body style="display:flex; align-items:center; margin:15%;">
+    <section>
+        <form action="includes/login.inc.php" method="POST">
+
+            <input type="text" name="uid" placeholder="Username">
+            <br>
+            <input type="password" name="pwd" placeholder="Password">
+            <br>
+            <button type="submit">LOGIN</button>
+        </form>
+    </section>
+    <section>
+        <form action="includes/signup.inc.php" method="POST">
+
+            <input type="text" name="uid" placeholder="Username">
+            <br>
+            <input type="password" name="pwd" placeholder="Password">
+            <br>
+            <input type="password" name="pwdrepeat" placeholder="Password">
+            <br>
+            <input type="email" name="email" placeholder="E-mail">
+            <br>
+            <button type="submit">SIGN UP</button>
+        </form>
+    </section>
+</body>
+
+</html>
