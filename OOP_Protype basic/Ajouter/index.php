997936ae457e956fe1744cd03d57973cbb43b566
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-12 09:33:19 +0100

    s
    s

--- OOP_Protype basic/Ajouter/index.php
+++ OOP_Protype basic/Ajouter/index.php
@@ -1,32 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Projet_1</title>
 </head>
-
 <body>
     <center>
         <h2>Add Promotion</h2>
-
         <form method="post">
             
                 <label> Promotion name:</label>
                 <input type="text"/>
                 <button type="submit"> Envoyer</button>
+
         </form>
-    </center>
-
-    <br />
-
-    <div class="col-lg-12">
+        <br />
         <div id="message"></div>
-        <div id="fetch"></div>
-    </div>
-
+    </center>
 </body>
-
 </html>
