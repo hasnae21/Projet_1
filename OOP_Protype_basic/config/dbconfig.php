c999ccc1821416bc396685f97067199fe9be2b80
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-12 13:19:13 +0100

    save
    save

--- OOP_Protype_basic/config/dbconfig.php
+++ OOP_Protype_basic/config/dbconfig.php
@@ -1,29 +1,24 @@
-
 <?php
 session_start();
 require_once('./config/operations.php');
 
 class dbconfig
 {
-    public $connection;
+    public $con;
 
-    public function __construct()
-    {
+    public function __construct(){
         $this->db_connect();
     }
 
-    public function db_connect()
-    {
-        $this->connection = mysqli_connect('localhost', 'root', '', 'projet_1');
+    public function db_connect(){
+        $this->con = mysqli_connect('localhost', 'root', '', 'projet_1');
         if (mysqli_connect_error()) {
-            die(" Connection Failed ");
+            die(" Database Connection Failed ):");
         }
     }
 
-    public function check($a)
-    {
-        $return = mysqli_real_escape_string($this->connection, $a);
+    public function check($a){
+        $return = mysqli_real_escape_string($this->con, $a);
         return $return;
     }
 }
-?>
--- OOP_Protype_basic/config/operations.php
+++ OOP_Protype_basic/config/operations.php
@@ -1,101 +1,109 @@
-<?php 
+<?php
 
-    require_once('./config/dbconfig.php');
-    $db = new dbconfig();
+require_once('./config/dbconfig.php');
+$db = new dbconfig();
 
-    class operations extends dbconfig
+class operations extends dbconfig
+{
+    // Insert Record in the Database
+    public function Store_Record()
     {
-        // Insert Record in the Database
-        public function Store_Record()
-        {
-            global $db;
-            if(isset($_POST['btn_save'])){
-                $promo_name = $db->check($_POST['promo_name']);
+        global $db;
+        if (isset($_POST['btn_save'])) {
+            $promo_name = $db->check($_POST['promo_name']);
 
-                if($this->insert_record($promo_name)){
-                    echo '<div class="alert alert-success"> Your Record Has Been Saved :) </div>';
-                }
-                else{
-                    echo '<div class="alert alert-danger"> Failed ): </div>';
-                }
+            if ($this->insert_record($promo_name)) {
+                echo '<div class="alert alert-success"> Your Record Has Been Saved :) </div>';
+            } else {
+                echo '<div class="alert alert-danger"> Failed ): </div>';
             }
         }
+    }
 
-      // Insert Record in the Database Using Query    
-        function insert_record($a)
-        {
-            global $db;
-            $query = "insert into promotion (name) values('$a')";
-            $result = mysqli_query($db->connection,$query);
+    // Insert Record in the Database Using Query    
+    function insert_record($a)
+    {
+        global $db;
+        $query = "insert into promotion (name) values('$a')";
+        $result = mysqli_query($db->connection, $query);
 
-            if($result){
-                return true;
-            }
-            else{
-                return false;
-            }
+        if ($result) {
+            return true;
+        } else {
+            return false;
         }
+    }
 
-        //View Database Record
-        public function view_record()
-        {
-            global $db;
-            $query = "select * from promotion";
-            $result = mysqli_query($db->connection,$query);
-            return $result;
-        }
+    //View Database Record
+    public function view_record()
+    {
+        global $db;
+        $query = "select * from promotion";
+        $result = mysqli_query($db->connection, $query);
+        return $result;
+    }
 
-        //Get Particular Record
-        public function get_record($id)
-        {
-            global $db;
-            $sql = "select * from promotion where ID='$id'";
-            $data = mysqli_query($db->connection,$sql);
-            return $data;
+    //Get Particular Record
+    public function get_record($id)
+    {
+        global $db;
+        $sql = "select * from promotion where ID='$id'";
+        $data = mysqli_query($db->connection, $sql);
+        return $data;
+    }
+
+
+    // Update Record
+    public function update()
+    {
+        global $db;
+
+        if (isset($_POST['btn_update'])) {
+            $ID = $_POST['ID'];
+            $FirstName = $db->check($_POST['First']);
+            $LastName = $db->check($_POST['Last']);
+            $UserName = $db->check($_POST['UserName']);
+            $Email = $db->check($_POST['UserEmail']);
+
+            if ($this->update_record($ID, $FirstName, $LastName, $UserName, $Email)) {
+                $this->set_messsage('<div class="alert alert-success"> Your Record Has Been Updated : )</div>');
+                header("location:view.php");
+            } else {
+                $this->set_messsage('<div class="alert alert-success"> Something Wrong : )</div>');
+            }
         }
     }
 
 
-        // Update Record
-        public function update()
-        {
-            global $db;
-            
-            if(isset($_POST['btn_update']))
-            {
-                $ID = $_POST['ID'];
-                $FirstName = $db->check($_POST['First']);
-                $LastName = $db->check($_POST['Last']);
-                $UserName = $db->check($_POST['UserName']);
-                $Email = $db->check($_POST['UserEmail']);
-                
-                if($this->update_record($ID,$FirstName,$LastName,$UserName,$Email ))
-                {
-                    $this->set_messsage('<div class="alert alert-success"> Your Record Has Been Updated : )</div>');
-                header("location:view.php");
-                }
-          else
-          {   
-              $this->set_messsage('<div class="alert alert-success"> Something Wrong : )</div>');
-          }
-          
-          
-            }
-        } 
-    
-        
-        // Update Function 2
-        public function update_record($id,$first,$Last,$User,$Email)
-        {
-            global $db;
-            $sql = "update employees set FirstName='$first', LastName='$Last', UserName='$User', Email='$Email' where ID='$id'";
-            $result = mysqli_query($db->connection,$sql);
-            if($result)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+    // Update Function 2
+    public function update_record($id, $first, $Last, $User, $Email)
+    {
+        global $db;
+        $sql = "update employees set FirstName='$first', LastName='$Last', UserName='$User', Email='$Email' where ID='$id'";
+        $result = mysqli_query($db->connection, $sql);
+        if ($result) {
+            return true;
+        } else {
+            return false;
         }
+    }
+
+    // Set Session Message
+    public function set_messsage($msg)
+    {
+        if (!empty($msg)) {
+            $_SESSION['Message'] = $msg;
+        } else {
+            $msg = "";
+        }
+    }
+
+    // Display Session Message
+    public function display_message()
+    {
+        if (isset($_SESSION['Message'])) {
+            echo $_SESSION['Message'];
+            unset($_SESSION['Message']);
+        }
+    }
+}
