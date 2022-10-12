aa19c99a41ef3b8da3a6761a47b13f8a35b864fe
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-12 11:34:15 +0100

    s

--- OOP_Protype_basic/config/dbconfig.php
+++ OOP_Protype_basic/config/dbconfig.php
@@ -1,30 +1,29 @@
 
-<?php 
-    session_start();
-    require_once('./config/operations.php');
+<?php
+session_start();
+require_once('./config/operations.php');
 
-    class dbconfig
+class dbconfig
+{
+    public $connection;
+
+    public function __construct()
     {
-        public $connection;
+        $this->db_connect();
+    }
 
-        public function __construct()
-        {
-            $this->db_connect();
-        }
-       
-        public function db_connect()
-        {
-            $this->connection = mysqli_connect('localhost','root','','projet_1');
-            if(mysqli_connect_error())
-            {
-                die(" Connection Failed ");
-            }
-        }
-
-        public function check($a)
-        {
-            $return = mysqli_real_escape_string($this->connection,$a);
-            return $return;
+    public function db_connect()
+    {
+        $this->connection = mysqli_connect('localhost', 'root', '', 'projet_1');
+        if (mysqli_connect_error()) {
+            die(" Connection Failed ");
         }
     }
+
+    public function check($a)
+    {
+        $return = mysqli_real_escape_string($this->connection, $a);
+        return $return;
+    }
+}
 ?>
--- OOP_Protype_basic/index.php
+++ OOP_Protype_basic/index.php
@@ -18,11 +18,11 @@ $db = new operations();
         <h2>Add Promotion</h2>
         <div class="container">
             <?php $db->Store_Record(); ?>
-                <form method="post">
-                    <label> Promotion name:</label>
-                    <input type="text" name="promo_name" required>
-                    <button name="btn_save"> Envoyer</button>
-                </form>
+            <form method="post">
+                <label> Promotion name:</label>
+                <input type="text" name="promo_name" required>
+                <button name="btn_save"> Envoyer</button>
+            </form>
         </div>
     </center>
 </body>
