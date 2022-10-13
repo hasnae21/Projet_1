99dca9625e8141491baa02fff6906bce52d6476d
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-13 14:40:11 +0100

    s
    s

---
+++ OOP_Prototype_Premuim/classes/db.php
@@ -0,0 +1,24 @@
+<?php
+require_once('./classes/PromotionManager.php');
+
+////////////////////////////database name : projet_1 
+////////////////////////////table name : promotion (id, name)
+
+class Connection
+{
+    public $con;
+    public function __construct(){
+        $this->db_connect();
+    }
+    public function db_connect(){
+        $this->con = mysqli_connect('localhost', 'root', '', 'projet_1');
+
+        if (mysqli_connect_error()) {
+            die(" Database Connection Failed ):");
+        }
+    }
+    public function check($e){
+        $return = mysqli_real_escape_string($this->con, $e);
+        return $return;
+    }
+}
---
+++ OOP_Prototype_Premuim/edit.php
@@ -0,0 +1,35 @@
+<?php
+require('./classes/db.php');
+$promo = new Promotion();
+$id = $_GET['id'];
+$promo->update();
+$result = $promo->get_record($id);
+$data = mysqli_fetch_assoc($result);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Modifier</title>
+</head>
+
+<body>
+    <center>
+        <h2>Modifier Promotion</h2>
+
+        <!-- store updated data in database -->
+        <?php $promo->Store_Record(); ?>
+
+        <form method="POST" autocomplete="off">
+            <label> Nom de la promotion :</label>
+            <input type="hidden" name="id" value="<?php echo $data['id']; ?>">
+            <input type="text" name="promo_name" required value="<?php echo $data['name']; ?>">
+            <button name="btn_update"> Envoyer </button>
+        </form>
+
+    </center>
+</body>
+
+</html>
