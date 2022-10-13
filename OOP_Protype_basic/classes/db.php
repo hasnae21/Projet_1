2c47dc1b2bdf2465a97bb720ab8977dcfa16c8b5
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-13 13:10:54 +0100

    s
    s

--- OOP_Protype_basic/classes/db.php
+++ OOP_Protype_basic/classes/db.php
@@ -9,6 +9,7 @@ class Connection
     public function __construct(){
         $this->db_connect();
     }
+
     public function db_connect(){
         $this->con = mysqli_connect('localhost', 'root', '', 'projet_1');
         
@@ -16,6 +17,7 @@ class Connection
             die(" echec de la connection au database  ):");
         }
     }
+    
     public function check($e){
         $return = mysqli_real_escape_string($this->con, $e);
         return $return;
