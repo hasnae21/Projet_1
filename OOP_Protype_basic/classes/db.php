8ce61f5307abbc20c782eb473366d593620e075f
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-13 13:12:27 +0100

    s
    s

--- OOP_Protype_basic/classes/db.php
+++ OOP_Protype_basic/classes/db.php
@@ -1,10 +1,14 @@
 <?php
 require_once('./classes/PromotionManager.php');
 
+
 ////////////////////////////database name : projet_1 
 ////////////////////////////table name : promotion (id, name)
+
+
 class Connection
 {
+
     public $con;
     public function __construct(){
         $this->db_connect();
@@ -17,9 +21,10 @@ class Connection
             die(" echec de la connection au database  ):");
         }
     }
-    
+
     public function check($e){
         $return = mysqli_real_escape_string($this->con, $e);
         return $return;
     }
+
 }
