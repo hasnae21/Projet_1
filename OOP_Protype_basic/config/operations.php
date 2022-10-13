90858b5119b9499df2c539178531e78375e9a74c
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-13 09:23:10 +0100

    Update operations.php

--- OOP_Protype_basic/config/operations.php
+++ OOP_Protype_basic/config/operations.php
@@ -5,7 +5,7 @@ $db = new dbconfig();
 class operations extends dbconfig
 {
 
-    // Insert Record in the Database
+    /////////////////////////////////// Insert Record in the Database
     public function Store_Record(){
 
         global $db;
@@ -22,7 +22,7 @@ class operations extends dbconfig
         }
     }
 
-    // Insert Record in the Database Using Query    
+    //////////////////////////////// Insert Record in the Database Using Query    
     function insert_record($r){
 
         global $db;
@@ -37,7 +37,7 @@ class operations extends dbconfig
         }
     }
 
-    //View Database Record
+    //////////////////////////////////View Database Record
     public function view_record(){
 
         global $db;
@@ -46,7 +46,8 @@ class operations extends dbconfig
         $result = mysqli_query($db->con, $query);
         return $result;
     }
-    //Get Particular Record
+
+    /////////////////////////////////////Get Particular Record
     public function get_record($id){
 
         global $db;
@@ -56,59 +57,32 @@ class operations extends dbconfig
         return $data;
     }
 
-
-
-        // Delete Record
-        public function Delete_Record($id)
-        {
-            global $db;
-            $query = "delete from promotion where  id='$id'";
-            $result = mysqli_query($db->con,$query);
-            if($result)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-      
-
-
-
-
-
-// Set Session Message
-public function set_messsage($msg){
-
-    if (!empty($msg)) {
-        $_SESSION['Message'] = $msg;
-    } else {
-        $msg = "";
-    }
-}
-// Display Session Message
-public function display_message(){
-
-    if (isset($_SESSION['Message'])) {
-
-        echo $_SESSION['Message'];
-        unset($_SESSION['Message']);
-    }
-}
-
-// Update Record
-public function update(){
+    ///////////////////////////////////////// Delete Record
+    public function Delete_Record($id)
+    {
         global $db;
+        $query = "delete from promotion where  id='$id'";
+        $result = mysqli_query($db->con,$query);
+        if($result){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    /////////////////////////////////////////// Update Record
+    public function update(){
 
+        global $db;
+    
         if (isset($_POST['btn_update'])) {
 
             $id = $_POST['id'];
             $name = $db->check($_POST['promo_name']);
-
+            
             if ($this->update_record($id, $name)) {
+
                 $this->set_messsage('<div> Your Record Has Been Updated :) </div>');
                 header("location:./view.php");
             } 
@@ -116,22 +90,39 @@ public function update(){
                 $this->set_messsage('<div> Something Wrong ): </div>');
             }
         }
-}
-public function update_record($id, $name){
+    }
+    public function update_record($id, $name){
         global $db;
-
+        
         $sql = "update promotion set name='$name' where id='$id'";
         $result = mysqli_query($db->con, $sql);
-
+        
         if ($result) {
             return true;
         } 
         else {
             return false;
         }
-}
-
-
-
-
+    }
+    
+    ///////////////////////////////////////////////////////validation messages
+    /////////////////////////////////////////////////////// Set Session Message
+    public function set_messsage($msg){
+    
+        if (!empty($msg)) {
+            $_SESSION['Message'] = $msg;
+        } else {
+            $msg = "";
+        }
+    }
+    ////////////////////////////////////////////////////// Display Session Message
+    public function display_message(){
+    
+        if (isset($_SESSION['Message'])) {
+    
+            echo $_SESSION['Message'];
+            unset($_SESSION['Message']);
+        }
+    }
+    
 }
