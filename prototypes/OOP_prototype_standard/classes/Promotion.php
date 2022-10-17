adff35c225565ccd42c34b5131c66b427ee51db6
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-10-17 16:45:27 +0100

    save
    save

--- prototypes/OOP_prototype_standard/classes/Promotion.php
+++ prototypes/OOP_prototype_standard/classes/Promotion.php
@@ -1,19 +1,24 @@
 <?php
-class Promo
-{
-    public $id;
-    public $name;
+class promotion {
 
-    public function set_name(){
+    private $id;
+    private $Name;
 
+    public function getId (){
+        return $this->Id;
     }
-    public function get_name(){
 
+    public function setId($id){
+        $this->Id = $id;
     }
-    public function set_id(){
 
+    public function getName (){
+        return $this->Name;
     }
-    public function get_name(){
 
+    public function setName($Name){
+        $this->Name = $Name;
     }
+
+
 }
